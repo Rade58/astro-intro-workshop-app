8cbc0634cb089b4627b48e97f1f7b00201ba15a1
Rade58 <bajic.rade2@gmail.com>
2023-07-30 22:07:57 +0200

    content scheema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,13 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
+
+// z is zod
+
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string().max(200),
+    }),
+  }),
+};
