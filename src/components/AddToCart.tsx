b894bbd896919c10eb5cf0ccd699b2e427ef8496
Rade58 <bajic.rade2@gmail.com>
2023-07-30 19:24:32 +0200

    react and solid components

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,3 +1,12 @@
 /** @jsxImportSource react */
-// This component is react component
+
 import { addItemToCart } from '../store/cart';
+
+// our component is react component
+export default function AddToCart({ item }: { item: ShopItem }) {
+  return (
+    <button className="big-link" onClick={() => addItemToCart(item)}>
+      Add To Cart
+    </button>
+  );
+}
