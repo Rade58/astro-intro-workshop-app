1fece293069f8119888fd89ec7d86a65fd245096
Rade58 <bajic.rade2@gmail.com>
2023-07-31 19:17:40 +0200

    generic

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,16 +12,15 @@ export default defineConfig({
   // don't forget to set this up to valid url
   //
   // check it on /rss.xml (we named endpoint like that inside pages folder)
-  site: 'https://radedev.com',
+  site: 'https://astro-workshop-sndwch.netlify.app/rss.xml',
   // this is the part of configuration where we say
   // that our site should be statically rendered (which is default)
   // but we need to set this if we want to be able to opt in into
   // ssr for certain pages we want
-  output: 'hybrid'
+  output: 'hybrid',
   // I think this is not experimental anymore
   // so I'm going to ommit this
   /* experimental: {
     hybridOutput: true,
-  }, */,
-  adapter: netlify()
+  }, */ adapter: netlify(),
 });
